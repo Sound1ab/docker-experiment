b68b694ee97fb660f08888c7751b4027c7431102
Phillip Parker <phillip-parker@outlook.com>
2019-04-05 11:02:11 +0100

    feat(Typorm): Setup typeorm connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,21 @@
 import 'reflect-metadata'
+import { createConnection } from 'typeorm'
 import express from 'express'
-import { createConnection } from 'typeorm/browser'
+import { config } from './config'
 
-createConnection().then(async connection => {
-  const PORT = process.env.PORT || 8088
-  const app = express()
+createConnection(config as any)
+  .then(async connection => {
+    const PORT = process.env.PORT || 8088
+    const app = express()
 
-  app.set('env', process.env.APP_ENV)
+    app.set('env', process.env.APP_ENV)
 
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`)
+    })
+
+    app.get('/', function(req, res) {
+      res.send('hello world')
+    })
   })
-})
+  .catch(error => console.log(error))
